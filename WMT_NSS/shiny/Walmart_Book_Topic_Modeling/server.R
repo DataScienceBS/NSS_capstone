110e4394e57c3ef4138582879dbf27d1701c3708
Brandon Sanders <DataScienceBS@gmail.com>
2018-07-21 14:01:47 -0500

    shiny app done. project complete

--- WMT_NSS/shiny/Walmart_Book_Topic_Modeling/server.R
+++ WMT_NSS/shiny/Walmart_Book_Topic_Modeling/server.R
@@ -1,9 +1,21 @@
-
 library(shiny)
+library(dplyr)
+library(highcharter)
+
 
 shinyServer(function(input, output) {
 
-# displaying word cloud  
+#### coherence img ####
+  output$coherenceImage <- renderImage({
+    coherencefile <- normalizePath(file.path('./images','coherence.png', sep=''))
+    
+    list(src = coherencefile,
+         alt = ("Coherence Model"))
+    
+  }, deleteFile = FALSE)
+  
+  
+#### displaying word cloud ####
   output$prepareImage <- renderImage({
     filename <- normalizePath(file.path('./images',
                                         paste('topic_', input$topic, '.png', sep='')))
@@ -14,28 +26,50 @@ shinyServer(function(input, output) {
     
   }, deleteFile = FALSE)
 
-# displaying pyLDAvis HTML files
+#### topic performance chart ####
+  topics <- c(12, 15, 17, 20, 23, 25, 28, 30, 32, 35, 37)
+  coherence <- c(0.5800,0.5800,0.5800, 0.5968, 0.6016, 0.5992, 0.5800, 0.6272, 0.5840, 0.5780,0.6055)
+  perplexity <- c(-8.6222,-8.6222,-8.6222,-8.6538,-8.6508,-8.6222,-8.6175,-8.6095,-8.6117,-8.6202,-8.6380)
+  
+  hc <-  highchart() %>% 
+    hc_title(text = "Model performance for a given number of topics") %>% 
+    hc_yAxis_multiples(
+      list(lineWidth = 3, title = list(text = "Coherence")),
+      list(opposite = TRUE, title = list(text = "Perplexity"))
+    ) %>% 
+    hc_add_series(data = coherence, name = "Coherence", color="blue") %>% 
+    hc_add_series(data = perplexity, name = "Perplexity", color="black", type = "spline", yAxis = 1) %>% 
+    hc_xAxis(categories = topics, title = list(text = "Number of topics in the trained model")) 
+  
+  
+  output$performance <- renderHighchart({hc})
+  
+  
+#### displaying pyLDAvis HTML files ####
   htmlname <- function(){
     return(includeHTML(paste0("html/vis_",input$topic_size_selection,".html")))
   }
   output$topic_selected_html <- renderUI({htmlname()})
 
-# text for background section  
+
+  
+#### text for background section ####
   output$background <- renderUI({
     HTML(paste(h3("Walking through the project"), br(),
                h4("Data Collection"),  
-               "The goal of this project was to access Walmart's Online Catalog using the ", tags$a(href = "https://developer.walmartlabs.com/", "Walmart Open API"), ". Because of the API limitations of 5,000 calls per day, I set this up in R so that I could run it daily in the background while working with small datasets separately in Python. Through data exploration, I identified the Book category ID and setup a process to loop through 1,285 book subcategories across the 50 main categories. This allowed for me to get a comprehensive list of titles for topic modeling.", br(),  
+               "The goal of this project was to access Walmart's Online Catalog using the ", tags$a(href = "https://developer.walmartlabs.com/", "Walmart Open API"), ". Because of the API limitations of 5,000 calls per day, I set this up in R so that I could run it daily in the background while working with small datasets separately in Python. Through data exploration, I identified the Book category ID and setup a process to loop through 1,285 book subcategories across the 50 main categories. This allowed for me to get a comprehensive list of titles for topic modeling.", br(), br(), 
 
                h4("Data Cleaning"),  
-               "There wasn't a huge need for data cleansing, just simple RegEx to remove html, convert contractions to words, and remove extra spaces. The text was also put in entire lowercase and duplicate records were removed. The duplicates were expected as some titles naturally fall into more than one topic (history + biography or memoirs, Law + Business + Education), and the API calls did indeed return titles with overlapping categories. This is expected to be reflected in topic modeling visuals.", br(),
+               "There wasn't a huge need for data cleansing, just simple RegEx to remove html, convert contractions to words, and remove extra spaces. The text was also put in entire lowercase and duplicate records were removed. The duplicates were expected as some titles naturally fall into more than one topic (history + biography or memoirs, Law + Business + Education), and the API calls did indeed return titles with overlapping categories. This is expected to be reflected in topic modeling visuals.", br(),br(),
 
                h4("Topic Modeling"),  
                "Topic modeling for this project became more of an art form than scientific process. How many topics should we consider? What chunksize is best for a good mix of speed and accuracy? What's an acceptable runtime? How many passes should the model be trained? Was 250k obsesrvations too large?", p(),
                "The first few model attempts with the full dataset were only about an hour of processing, using gensim's ldamodel. THEN, I found LdaMultiCore, and quickly realized more processor power definitely sped up the modeling process. BUT, I had concerns. I decided to calculate coherence values, and while the bigger datasets definitely improved, the LdaMulticore models were significantly worse than the LdaModel.", p(),
                "In the end, the best performing model was 30 topics (from a dataset containing 40 topics), with a chunksize of 50,000 and 50 passes. The final scores were a coherence value of 0.627 and perplexity of -8.609. Total runtime was between 2-3 hours. Reducing the passes to 20 saved significant time (slightly over 1 hour) with minimal change in the scoring.", p(),               
-
+               br(),
+               
                h4("Displaying Results"),
-               "Topic modeling gives insight on the salient words that help define common docs. pyLDAvis is an incredible package for visualizing the differences between topics, and it's completely interactive with only 2 lines of code. I decided to use word clouds to help visualize the topics, which is where I discovered pyLDAvis re-orders the topics and my word clouds weren't lining up. Reading through the documentation, a recent addition to the package included the sort_topics option to easily remedy this. Throwing the various topic sizes into a shiny app seemed like the best approach for giving the user ability to explore the data and determine the best application of topic modeling.", br(),
+               "Topic modeling gives insight on the salient words that help define common docs. pyLDAvis is an incredible package for visualizing the differences between topics, and it's completely interactive with only 2 lines of code. I decided to use word clouds to help visualize the topics, which is where I discovered pyLDAvis re-orders the topics and my word clouds weren't lining up. Reading through the documentation, a recent addition to the package included the sort_topics option to easily remedy this. Throwing the various topic sizes into a shiny app seemed like the best approach for giving the user ability to explore the data and determine the best application of topic modeling.", br(),br(),
                
                h4("Packages Used"),
                tags$ul(
--- WMT_NSS/shiny/Walmart_Book_Topic_Modeling/ui.R
+++ WMT_NSS/shiny/Walmart_Book_Topic_Modeling/ui.R
@@ -1,6 +1,6 @@
 library(shiny)
 library(shinythemes)
-
+library(highcharter)
 
 shinyUI(fluidPage(theme = shinytheme("flatly"),
                                     
@@ -12,32 +12,59 @@ navbarPage(" Walmart Books",
            uiOutput("background")
            ),
   tabPanel("Topic Sizes",
-           sidebarLayout(
-             column(3,sidebarPanel(
-               radioButtons("topic_size_selection",
-                           "Topic Size: ",
-                           choiceNames = c("12","17","20","23","25","28","30","32","35","37"),
-                           choiceValues = c("12","17","20","23","25","28","30","32","35","37"),
-                           selected = "12")
-             )),
-             
-             # Display the pyLDAvis HTML file
-             column(9,mainPanel(
-               htmlOutput("topic_selected_html")
-             )
-           ))),
+                fluidRow(
+                  h3("Select the number of topics you wish to categorize Walmart's book catalog:"),
+                  br(),
+                  column(1,
+                         radioButtons("topic_size_selection",
+                             "Topic Size: ",
+                             choiceNames = c("12","15","17","20","23","25","28","30","32","35","37"),
+                             choiceValues = c("12","15","17","20","23","25","28","30","32","35","37"),
+                             selected = "30")
+               ),
+               # Display the pyLDAvis HTML file
+                      column(11,
+                        htmlOutput("topic_selected_html")
+                   )
+           )),
+  tabPanel("Model Performance",
+           fluidRow(
+             column(4,
+                    p(),
+                    ("The performance of each topic was graded on two metrics, coherence and perplexity. See below for the technical details, but essentially you want a higher coherence and lower perplexity. Perplexity is a measure of how well the model describes a set of documents."), 
+                    p(), 
+                    ("As shown in the chart, a model with 30 topics resulted in the best performance. All models in this visual performed with either 40,000 or 50,000 chunksize and 50 passes using the gensim LDA model. Since this model appeared to perform best, word clouds were generated for each topic in this model to give a better visual.")),
+             column(8,
+                    highchartOutput("performance"))),
+           fluidRow(
+             column(4,
+             h3("Coherence score calculation:"),
+             tags$ul(),
+             tags$li("t : Topics coming in from the topic model"),
+             tags$li(" S : Segmented topics"),
+             tags$li("P : Calculated probilities"),
+             tags$li("Phi vector : A vector of the “confirmed measures” coming out from the confirmation module"),
+             tags$li("c : The final coherence value"),
+             "Source: ", tags$a(href = "http://svn.aksw.org/papers/2015/WSDM_Topic_Evaluation/public.pdf", "Exploring the Space of Topic Coherence Measures (pdf)")
+             ),
+             column(8,
+                    imageOutput("coherenceImage"))
+           )),
+  
+  
   tabPanel("Topic WordClouds",
-    sidebarLayout(
-      sidebarPanel(
+    fluidRow(
+      h4("Word clouds shown are for a 30-topic model, where size represents that words contribution to that topic. Color is purely aesthetic."),
+      column(3,
          sliderInput("topic",
-                     "Topics:",
+                     "Topic Cluster:",
                      min = 1,
                      max = 30,
                      value = 1)
     ),
     
     # Display word clouds for selected topic number
-    mainPanel(
+    column(9,
        imageOutput("prepareImage")
     )
   )
